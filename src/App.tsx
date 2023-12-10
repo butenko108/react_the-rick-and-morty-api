2057e357c44a6b5bb42e86c141584b045d3a20af
oleh108 <oleg.butenko@lumitech.co>
2023-12-10 16:08:15 +0200

    chore: favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+    <div>
+      <header>
         <h1 className="text-2xl font-bold text-red-600 underline">Simple React Typescript Tailwind Sample</h1>
       </header>
     </div>
