2e6b0a064795a437bd83bc185ee1694ae027414b
oleh108 <oleg.butenko@lumitech.co>
2023-12-11 14:07:09 +0200

    refactor: const CREATURE is moved

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,35 +4,8 @@ import { Stack } from '@mui/system';
 import { MaterialUIProvider } from './providers';
 import { EllipsisURL } from '../assets';
 import { Card } from '../features';
-import { Creature } from '../shared/types';
+import { CREATURES } from '../shared/constants';
 import { Container } from '../shared/ui/container/container';
-
-const creatures: Creature[] = [
-  {
-    name: 'Aqua Rick',
-    isAlive: 'Unknown',
-    type: 'Humanoid',
-    lastLocation: 'Citadel of Ricks',
-    firstSeen: 'Close Rick-counters of the Rick Kind',
-    image: 'https://4.img-dpreview.com/files/p/E~C667x0S5333x4000T1200x900~articles/3925134721/0266554465.jpeg',
-  },
-  {
-    name: 'Aqua Rick',
-    isAlive: 'Unknown',
-    type: 'Humanoid',
-    lastLocation: 'Citadel of Ricks',
-    firstSeen: 'Close Rick-counters of the Rick Kind',
-    image: 'https://4.img-dpreview.com/files/p/E~C667x0S5333x4000T1200x900~articles/3925134721/0266554465.jpeg',
-  },
-  {
-    name: 'Aqua Rick',
-    isAlive: 'Unknown',
-    type: 'Humanoid',
-    lastLocation: 'Citadel of Ricks',
-    firstSeen: 'Close Rick-counters of the Rick Kind',
-    image: 'https://4.img-dpreview.com/files/p/E~C667x0S5333x4000T1200x900~articles/3925134721/0266554465.jpeg',
-  },
-];
 
 function App() {
   return (
@@ -47,21 +20,21 @@ function App() {
             </Stack>
           </div>
 
-          <ul className="grid gap-[27px] sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2">
-            {creatures.map((creature, index) => (
+          <ul className="relative grid gap-[27px] sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2">
+            {CREATURES.map((creature, index) => (
               <li key={index}>
                 <Card card={creature} />
               </li>
             ))}
-          </ul>
 
-          <Button
-            variant="contained"
-            color="white"
-            className="!shadow-button !h-14 !min-w-[56px] !max-w-[56px] !rounded-full !p-0"
-          >
-            <img src={EllipsisURL} alt="Ellipsis" />
-          </Button>
+            <Button
+              variant="contained"
+              color="white"
+              className="!shadow-button !absolute -bottom-7 -right-7 !h-14 !min-w-[56px] !max-w-[56px] !rounded-full !p-0"
+            >
+              <img src={EllipsisURL} alt="Ellipsis" />
+            </Button>
+          </ul>
         </Container>
       </main>
     </MaterialUIProvider>
